5839bb6eca6020b716e43878a065eec3f97f5d09
Nguyen Van Minh <van.minh@grabtaxi.com>
2024-05-18 16:23:21 +0800

    Update version

--- build-logic/src/main/java/com/grab/sizer/buildplugins/AppSizerConfigPlugin.kt
+++ build-logic/src/main/java/com/grab/sizer/buildplugins/AppSizerConfigPlugin.kt
@@ -20,7 +20,7 @@ class AppSizerConfigPlugin : Plugin<Project> {
 
         project.the<MobilePublishExtension>().apply {
             groupId = "com.grab"
-            version = if (System.getenv("CI") != null) "SNAPSHOT-01" else "SNAPSHOT"
+            version = if (System.getenv("CI") != null) "SNAPSHOT-02" else "SNAPSHOT"
         }
 
         project.tasks.withType(KotlinCompile::class.java).forEach {
