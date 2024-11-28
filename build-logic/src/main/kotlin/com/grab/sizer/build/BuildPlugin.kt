e23f06faa3eb640f9744d2eb5b330cdd8424b141
arunkumar9t2 <arunkumar9t2@users.noreply.github.com>
2024-11-29 02:48:59 +0800

    Organize code and setup publishing

--- build-logic/src/main/kotlin/com/grab/sizer/build/BuildPlugin.kt
+++ build-logic/src/main/kotlin/com/grab/sizer/build/BuildPlugin.kt
@@ -25,31 +25,15 @@
  * SOFTWARE
  */
 
-package com.grab.sizer.buildplugin
+package com.grab.sizer.build
 
+import com.grab.sizer.gradle.ConfigurablePlugin
 import org.gradle.api.JavaVersion
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.tasks.compile.JavaCompile
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-/** This plugin represents a build configuration
- * of java-libraries/kotlin modules.
- */
+class BuildPlugin : ConfigurablePlugin({
 
-class AppSizerConfigPlugin : Plugin<Project> {
-    override fun apply(project: Project) {
-        project.plugins.apply("org.jetbrains.kotlin.jvm")
-        project.plugins.apply("org.jetbrains.kotlin.kapt")
-        project.version = if (System.getenv("CI") != null) "SNAPSHOT-08" else "SNAPSHOT"
-
-        project.tasks.withType(KotlinCompile::class.java).forEach {
-            it.kotlinOptions.jvmTarget = "11"
-        }
-
-        project.tasks.withType(JavaCompile::class.java).configureEach {
-            sourceCompatibility = JavaVersion.VERSION_11.toString()
-            targetCompatibility = JavaVersion.VERSION_11.toString()
-        }
-    }
-}
+})
